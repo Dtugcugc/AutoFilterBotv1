5c13ae465c9ae71f46f320f9e3519827498f08df
Dtugcugc <126472008+Dtugcugc@users.noreply.github.com>
2023-02-27 18:22:21 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = Abhishek Krishnan,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '💯our group AlAS💯', url="https://t.me/madjgdgdgdg"
                                 )
                         ]
                     ]
